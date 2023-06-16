a6e69172586eb7a16934a1b2867211cb8dbf8093
Jordana Clarimundo Dionisio <115229693+Jordana-Clarimundo@users.noreply.github.com>
2023-06-16 11:24:38 -0300

    flutter dart

---
+++ app_lista_tarefas/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:app_lista_tarefas/paginas/pagina_lista.dart';
+//todo app tem que fazer esses comandos (linha 7 até 11)
+void main() {
+  runApp(Meuapp());
+}
+class Meuapp extends StatelessWidget{
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //o parametro home e o que especifica a tela inicial do app
+      home: Pagina_Lista(),
+    );    
+  }
+}
---
+++ app_lista_tarefas/lib/paginas/pagina_lista.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Pagina_Lista extends StatelessWidget{
+  Widget build(BuildContext context) {
+    return Container(color: Colors.blueGrey,
+    );
+  }
+}
